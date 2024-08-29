1980f90217d837dbae98d56f2c8e665b88c8dbfd
Namanjeet13 <namanjeetsingh@academic.rrc.ca>
2024-08-29 13:21:13 -0500

    Variables

--- first.js
+++ first.js
@@ -1,1 +1,55 @@
-name = "Namanjeet Singh"
+// String
+let fullName = "Namanjeet Singh";
+console.log(fullName);
+
+// Numeric
+let age = 24;
+console.log(age);
+
+// Numeric
+let price = 99.99;
+console.log(price);
+
+// Null
+let x = null;
+console.log(x);
+
+// Undefined
+let y = undefined;
+console.log(y);
+
+// Boolean
+let isFollow = false;
+console.log(isFollow);
+
+// Change of variable
+fullName = "25";
+console.log(fullName);
+
+// Difference showing case sensitivity in variable Names
+let fullname = "Namanjeet Singh";
+console.log(fullname);
+
+// Default value = undefined
+let a;
+console.log(a);
+
+// Blocks
+{
+    let a = 5;
+    console.log(a);
+}
+{
+    let a = 10;
+    console.log(a);
+}
+
+// BigInt adds n at the end of numeric value
+let bigInt = BigInt("123");
+console.log(bigInt);
+
+// Rarely even used
+
+// Symbol prints Symbol(Hello123!)
+let symbol = Symbol("Hello123!");
+console.log(symbol);
