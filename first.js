a335a159c38373c3a80dccf1ff83aa36d706c2a4
Namanjeet13 <namanjeetsingh@academic.rrc.ca>
2024-08-29 13:31:16 -0500

    Objects

--- first.js
+++ first.js
@@ -1,2 +1,14 @@
-console.log("Namanjeet");
-console.log("Singh");
+// Object
+const student = {
+    fullName : "Namanjeet",
+    age : 19,
+    cgpa : 8.2,
+    isPass : true
+}
+
+student["age"] = student["age"] + 1; // Can Change any Key
+
+console.log(student);
+console.log(student["fullName"]); // Accesing key in Object
+console.log(student.fullName); // Accesing key in Object
+console.log(student.age);
